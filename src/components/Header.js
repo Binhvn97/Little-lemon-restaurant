9f518c1706be871fb3e46bac0885ec8e9b90a8be
Binhvn97 <hoangphamthanhbinh.510@gmail.com>
2024-07-06 02:52:20 +0700

    css and assets edit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom'
+
 import Nav from './Nav'
 
 
 function Header() {
     return (
         <header>
-            <h2>Hello</h2>
+            <Link to="/"><img src={require('../assets/logo.png')} alt="little lemon" /></Link>
             <Nav />
         </header>
     )
