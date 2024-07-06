2891ed35c2048c5ab2cdcc29106e1430491e6929
Binhvn97 <hoangphamthanhbinh.510@gmail.com>
2024-07-07 01:17:24 +0700

    update css

--- src/components/Description.js
+++ src/components/Description.js
@@ -3,7 +3,24 @@
 
 
 function Description() {
-
+    return (
+        <>
+            <div className="restaurant-desc-parent">
+                <div className="restaurant-desc">
+                    <div className="description-title">
+                        <p className="title desc-title">Little Lemon</p>
+                        <p className="subtitle desc-subtitle">Chicago</p>
+                    </div>
+                    <p className="desc-text desc">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
+                        Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+                        Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. </p>
+                </div>
+                <div className="desc-images">
+                    <img className="desc-img" src="/images/hero.png" alt="hero-image" />
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Description
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -8,10 +8,13 @@ function Testimonials() {
             <p className="testimonials-title">Testimonials</p>
             <div className="testimonials">
                 <div className="testimonial">
-                    <img className="reviewer-img" src="/images/reviewer1." alt="greek salad" />
+                    <div className="reviewer-rating">
+                        <p className="rate-title">Rating</p>
+                        <p className="rating-points">5/5</p>
+                    </div>
                     <div className="name-rating">
+                        <img className="reviewer-img" src="/images/reviewer1.jpg" alt="greek salad" />
                         <p className="reviewer-name">John Doe</p>
-                        <p className="reviewer-rating">5/5</p>
                     </div>
                     <div className="review-text">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec massa massa,
@@ -20,10 +23,13 @@ function Testimonials() {
                     </div>
                 </div>
                 <div className="testimonial">
-                    <img className="reviewer-img" src="/images/reviewer2.png" alt="greek salad" />
+                    <div className="reviewer-rating">
+                        <p className="rate-title">Rating</p>
+                        <p className="rating-points">5/5</p>
+                    </div>
                     <div className="name-rating">
+                        <img className="reviewer-img" src="/images/reviewer2.png" alt="greek salad" />
                         <p className="reviewer-name">John Doe</p>
-                        <p className="reviewer-rating">5/5</p>
                     </div>
                     <div className="review-text">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec massa massa,
@@ -32,10 +38,28 @@ function Testimonials() {
                     </div>
                 </div>
                 <div className="testimonial">
-                    <img className="reviewer-img" src="/images/reviewer3.jpg" alt="greek salad" />
+                    <div className="reviewer-rating">
+                        <p className="rate-title">Rating</p>
+                        <p className="rating-points">5/5</p>
+                    </div>
                     <div className="name-rating">
+                        <img className="reviewer-img" src="/images/reviewer3.jpg" alt="greek salad" />
                         <p className="reviewer-name">John Doe</p>
-                        <p className="reviewer-rating">5/5</p>
+                    </div>
+                    <div className="review-text">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec massa massa,
+                            semper sed enim at, faucibus mattis lorem. Donec vehicula, leo ut auctor ullamcorper,
+                            diam tortor dignissim velit, ac iaculis libero lacus non diam. </p>
+                    </div>
+                </div>
+                <div className="testimonial">
+                    <div className="reviewer-rating">
+                        <p className="rate-title">Rating</p>
+                        <p className="rating-points">5/5</p>
+                    </div>
+                    <div className="name-rating">
+                        <img className="reviewer-img" src="/images/reviewer4.jpg" alt="greek salad" />
+                        <p className="reviewer-name">John Doe</p>
                     </div>
                     <div className="review-text">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec massa massa,
