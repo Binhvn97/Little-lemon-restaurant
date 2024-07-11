a506aaa31eb2558a19b7e15ec637da9f25267e4c
Binhvn97 <hoangphamthanhbinh.510@gmail.com>
2024-07-11 22:42:08 +0700

    update

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -25,49 +25,84 @@ function BookingForm(availableTimes) {
 
 
     return (
-        <main>
+        <main className="reservation-wrap">
             <p className="desc-text form-desc">Please fill in the form below accurately to enable us serve you nicely</p>
             <form onSubmit={handleSubmit}>
                 <div className="separate">
                     <label htmlFor="first-name">First Name</label>
-                    <input type="text" id="first-name" name="firstName" value={formData.firstName} onChange={handleFormChange} required />
+                    <input
+                        type="text"
+                        id="first-name"
+                        name="firstName"
+                        placeholder="Enter your first name..."
+                        value={formData.firstName}
+                        onChange={handleFormChange}
+                        required
+                    />
                 </div>
                 <div className="separate">
                     <label htmlFor="last-name">Last Name</label>
-                    <input type="text" id="last-name" name="lastName" value={formData.lastName} onChange={handleFormChange} required />
+                    <input
+                        type="text"
+                        id="last-name"
+                        name="lastName"
+                        placeholder="Enter your last name..."
+                        value={formData.lastName}
+                        onChange={handleFormChange}
+                        required
+                    />
                 </div>
                 <div className="separate">
                     <label htmlFor="contact-number">Contact Number</label>
-                    <input type="text" id="contact-number" name="contactNumber" placeholder="123-456-7890"
+                    <input
+                        type="text"
+                        id="contact-number"
+                        name="contactNumber"
+                        placeholder="Enter your contact number..."
                         value={formData.contactNumber} onChange={handleFormChange}
-                        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" />
+                        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                    />
                 </div>
-                <div className="separate date-time">
-                    <div className="separate-child">
+                <div className="date-time">
+                    <div className="separate separate-child">
                         <label htmlFor="res-date">Choose date</label>
-                        <input type="date" id="res-date" name="date" value={formData.date} onChange={handleDateChange} required min={currentDate} />
+                        <input
+                            type="date"
+                            id="res-date"
+                            name="date"
+                            value={formData.date}
+                            onChange={handleDateChange}
+                            required min={currentDate}
+                        />
                     </div>
-                    <div className="separate-child">
+                    <div className="separate separate-child">
                         <label htmlFor="res-time">Choose time</label>
-                        <select id="res-time " name="time" value={formData.time} onChange={handleFormChange} required>
+                        <select
+                            id="res-time "
+                            name="time"
+                            value={formData.time}
+                            onChange={handleFormChange}
+                            required
+                        >
 
                         </select>
                     </div>
                 </div>
-                <div className="separate guests-occasion">
-                    <div className="separate-child">
+                <div className="guests-occasion">
+                    <div className="separate separate-child">
                         <label htmlFor="guests">Number of guests</label>
                         <input type="number" placeholder="1" min="1" max="10" required id="guests" name="noOfGuests" value={formData.noOfGuests} onChange={handleFormChange} />
                     </div>
-                    <div className="separate-child">
+                    <div className="separate separate-child">
                         <label htmlFor="occasion">Occasion</label>
                         <select id="occasion" name="occasion" required value={formData.occasion} onChange={handleFormChange}>
                             <option>Birthday</option>
                             <option>Anniversary</option>
+                            <option>Engagement</option>
                         </select>
                     </div>
                 </div>
-                <input className="submit" type="submit" value="Reserve" aria-label="submit button" />
+                <button className="btn">Reserve</button>
             </form>
         </main>
     )
